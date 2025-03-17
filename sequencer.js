22ead8e0972e3b3022d03434d63c6034f57f4a9c
hase <62376587+patrickahase@users.noreply.github.com>
2025-03-17 12:41:42 +1100

    middle of implementing step seq
    got stuck getting html indeterminate to work - need bigger screen to trouble shoot

--- sequencer.js
+++ sequencer.js
@@ -1,11 +1,8 @@
+/* basic synth for note input */
+
 const synth = new Tone.Synth().toDestination();
 
-const seq = new Tone.Sequence((time, note) => {
-  synth.triggerAttackRelease(note, 0.1, time);
-  // subdivisions are given as subarrays
-}, ["C4", ["E4", "D4", "E4"], "G4", ["A4", "G4"]]).start(0);
-
-seq.probability = 0.8;
+/* transport set up : will add bpm later */
 
 let transportToggle = document.getElementById("transport-toggle");
 
@@ -22,7 +19,50 @@ transportToggle.addEventListener("click", () => {
   isPlaying = !isPlaying;
 });
 
-const loop = new Tone.Loop((time) => {
+/* callback loop */
+
+const loop = new Tone.Loop(() => {
   // triggered every eighth note.
-  console.log(time);
+  nextStep();
 }, "8n").start(0);
+
+/* array with current active steps */
+
+let currentSequence = [];
+
+let currentStep = 0;
+
+/* find inputs and add eventlistener and map to sequence array */
+
+let stepBoxes = Array.from(document.getElementsByClassName("seqStepBox"));
+
+stepBoxes.forEach((stepBox, index) => {
+
+  currentSequence.push({
+    stepIndex : index,
+    note: "C4",
+    active: stepBox.checked,
+    inputElement: stepBox,
+    reset: index === stepBoxes.length-1 ? false : stepBoxes[index+1].indeterminate
+  });
+
+  stepBox.addEventListener("click", () => {
+    if(stepBox.readOnly) stepBox.checked = stepBox.readOnly = false;
+    else if(!stepBox.checked) stepBox.readOnly = stepBox.indeterminate = true;
+  });
+});
+
+function updateStepStyle(){
+  if (currentStep === 0){
+    stepBoxes[0].style.backgroundColor = 'red';
+    stepBoxes[stepBoxes.length-1].style.backgroundColor = null;
+  } else {
+    stepBoxes[currentStep].style.backgroundColor = 'red';
+    stepBoxes[currentStep-1].style.backgroundColor = null;
+  } 
+  if (currentStep === stepBoxes.length-1) {
+    currentStep = 0;
+  } else {
+    currentStep ++;
+  }
+}
