78b20059b5cae928b5614658a182ea64934a78ee
Patrick McMahon <patrickahase@gmail.com>
2025-03-17 14:24:40 +1100

    kind of got it working with the sequencer

--- sequencer.js
+++ sequencer.js
@@ -21,9 +21,9 @@ transportToggle.addEventListener("click", () => {
 
 /* callback loop */
 
-const loop = new Tone.Loop(() => {
+const loop = new Tone.Loop((time) => {
   // triggered every eighth note.
-  nextStep();
+  nextStep(time);
 }, "8n").start(0);
 
 /* array with current active steps */
@@ -36,31 +36,52 @@ let currentStep = 0;
 
 let stepBoxes = Array.from(document.getElementsByClassName("seqStepBox"));
 
-stepBoxes.forEach((stepBox, index) => {
+let stepInputs = Array.from(document.getElementsByClassName("seqStep"));
+
+stepInputs.forEach((stepInput, index) => {
 
   currentSequence.push({
     stepIndex : index,
     note: "C4",
-    active: stepBox.checked,
-    inputElement: stepBox,
-    reset: index === stepBoxes.length-1 ? false : stepBoxes[index+1].indeterminate
+    active: stepInput.checked,
+    inputElement: stepInput,
+    boxElement: stepBoxes[index],
+    reset: index === stepInputs.length-1 ? true : stepInputs[index+1].indeterminate
   });
 
-  stepBox.addEventListener("click", () => {
-    if(stepBox.readOnly) stepBox.checked = stepBox.readOnly = false;
-    else if(!stepBox.checked) stepBox.readOnly = stepBox.indeterminate = true;
+  stepInput.addEventListener("click", () => {
+    if(stepInput.readOnly) {
+      stepInput.checked = stepInput.readOnly = false;
+      currentSequence[index].active = false;
+    } else if(!stepInput.checked) {
+      stepInput.readOnly = stepInput.indeterminate = true;
+      currentSequence[index].active = false;
+      if (index !== 0) {
+        currentSequence[index-1].reset = true;
+      }
+      currentSequence[index].active = false;
+    } else {
+      currentSequence[index].active = true;
+    }
   });
 });
 
-function updateStepStyle(){
+function nextStep(time){
+  /* play note */
+  if(currentSequence[currentStep].active){
+    synth.triggerAttackRelease(currentSequence[currentStep].note, "8n", time);
+  }
+
+  /* style */
   if (currentStep === 0){
-    stepBoxes[0].style.backgroundColor = 'red';
-    stepBoxes[stepBoxes.length-1].style.backgroundColor = null;
+    currentSequence[0].boxElement.classList.add("activeStep");
+    /* store this as a precomputed var */
+    currentSequence[currentSequence.length-1].boxElement.classList.remove("activeStep");
   } else {
-    stepBoxes[currentStep].style.backgroundColor = 'red';
-    stepBoxes[currentStep-1].style.backgroundColor = null;
+    currentSequence[currentStep].boxElement.classList.add("activeStep");
+    currentSequence[currentStep-1].boxElement.classList.remove("activeStep");
   } 
-  if (currentStep === stepBoxes.length-1) {
+  if (currentSequence[currentStep].reset) {
     currentStep = 0;
   } else {
     currentStep ++;
